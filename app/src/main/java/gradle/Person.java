440f80cf0bd43c00bf9409dc3d9e6b2e60b4383d
Eugen2510 <Lugovoj20@gmail.com>
2023-06-18 21:44:30 +0300

    add gradle task

---
+++ app/src/main/java/gradle/Person.java
@@ -0,0 +1,9 @@
+package gradle;
+
+import lombok.Data;
+
+@Data
+public class Person {
+    private String name;
+    private String lastname;
+}
---
+++ app/src/main/java/gradle/PersonNameShower.java
@@ -0,0 +1,13 @@
+package gradle;
+
+import com.google.gson.Gson;
+
+public class PersonNameShower {
+    public static void main (String [] args){
+        Person person = new Person();
+        person.setName("Eugene");
+        person.setLastname("Luhovyi");
+
+        System.out.println(new Gson().toJson(person));
+    }
+}
